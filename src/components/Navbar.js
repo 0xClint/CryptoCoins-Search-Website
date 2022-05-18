6224f4bef9349ab63fec0be510fcf420878e9de7
Omkar <omkarbdarde@gmail.com>
2022-05-18 20:06:19 +0530

    make it better

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaCoins } from "react-icons/fa";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <Link to="/">
+        <div className="navbar">
+          <FaCoins className="icon" />
+          <h1>
+            Crypto<span className="purple">Coins</span>
+          </h1>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default Navbar;
